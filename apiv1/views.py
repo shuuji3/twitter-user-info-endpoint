3a8d0e9ad033d02300dad265e93d43ce18597ce7
TAKAHASHI Shuuji <shuuji3@gmail.com>
2018-05-26 23:57:59 +0900

    Show the error page for the invalid credentials or screen_name.

--- apiv1/views.py
+++ apiv1/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseServerError
 from apiv1.utils import resolve_user_icon_url
 
 
 def user_icon(request, screen_name: str):
     img_url = resolve_user_icon_url(screen_name)
+    if not img_url:
+        html = '<h1>500 Server Error</h1><p>Either <b>Twitter credentials</b> or <b>screen_name</b> seems to be invalid.</p>'
+        return HttpResponseServerError(html)
     return HttpResponseRedirect(img_url)
